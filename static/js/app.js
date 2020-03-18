226fc7d88a7cd8cec081a596d4d7d3cb32861064
David Jaimes <david@djaimes.com>
2020-03-17 23:04:09 -0700

    finish simplifying code and add hover text

--- static/js/app.js
+++ static/js/app.js
@@ -1,56 +1,54 @@
-// Save file name to variable
-var fname = "./samples.json"
-
-// Create dropdown menu from individual IDs.
-var data = d3.json(fname).then(function(data) {
-  var names = data.names
-  names.forEach(n => {
+// Create dropdown menu from individual IDs to index html file.
+d3.json("./samples.json").then(data => {
+  data.names.forEach(n => {
     d3.select("#selDataset")
     .append("option")
     .text(n)
     .property("value", n)
   });
-});
 
-// Display the default plot
-function defaultPlot(data) {
-
-  d3.json(fname).then(function(data) {
-
-    var id = "940";
-    var ind = data.names.findIndex(d => d === id);
-    var values = data.samples[ind].sample_values.slice(0, 10);
-    var otu_ids = data.samples[ind].otu_ids.slice(0, 10);
-    var otu_labels = data.samples[ind].otu_labels.slice(0, 10);
-
-    var data = [{
-      x: values.reverse(),
-      y: otu_ids.map(d => `OTU ${d}`).reverse(),
-      type: "bar",
-      orientation: "h"
-    }];
-
-    Plotly.newPlot("bar", data);
-  });
-};
-
-// On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", optionChanged);
-
-// Function called by DOM changes
-function optionChanged() {
-  var dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  var dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
-  d3.json(fname).then(function(data) {
-    var ind = data.names.findIndex(d => d === dataset);
+  // Select data from samples.json file and return object with top ten
+  // UTOs found in individual.
+  function selectData(index) {
+    // PARAMETERS
+    // x: sample_values, y: otu_ids, l: otu_labels
+    var ind = data.names.findIndex(d => d === index);
     var x = data.samples[ind].sample_values.slice(0, 10);
-    var y = otu_ids = data.samples[ind].otu_ids.slice(0, 10);
+    var y = data.samples[ind].otu_ids.slice(0, 10);
+    var l = data.samples[ind].otu_labels.slice(0, 10);
+    return {
+      x: x.reverse(),
+      y: y.map(d => `OTU ${d}`).reverse(),
+      l: l.reverse()};
+  };
 
-    Plotly.restyle("bar", "x", [x.reverse()]);
-    Plotly.restyle("bar", "y", [y.map(d => `OTU ${d}`).reverse()]);
-    });
-}
+  // Display the default plot
+  function defaultPlot() {
+    var selection = selectData("940");
+    var barData = [{
+      x: selection.x,
+      y: selection.y,
+      type: "bar",
+      orientation: "h",
+      hovertext: selection.l
+    }];
+    Plotly.newPlot("bar", barData);
+  };
 
-defaultPlot();
+  // On change to the DOM, call optionChanged()
+  d3.selectAll("#selDataset").on("change", optionChanged);
+
+  // Function called by DOM changes
+  function optionChanged() {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
+    var sel = selectData(dataset);
+
+    Plotly.restyle("bar", "x", [sel.x]);
+    Plotly.restyle("bar", "y", [sel.y]);
+
+  }
+
+  defaultPlot();
+});
